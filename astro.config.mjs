27bac57982d563a47db7a0c7184b16ca7be4341f
John Solly <9572232+jsolly@users.noreply.github.com>
2024-10-01 12:43:49 -0400

    fix import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
-import vercel from "@astrojs/vercel/serverless";
+import vercelServerless from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
