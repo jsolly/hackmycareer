d79af4e7303c2acbd017b712fdcac1247d6b86e9
John Solly <9572232+jsolly@users.noreply.github.com>
2024-10-01 12:28:34 -0400

    Add vercel dep

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
 	site: "https://hackmycareer.lol",
 	integrations: [tailwind()],
+	output: "server",
+	adapter: vercel(),
 });
