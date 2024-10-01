976adc82d02ece4125be87c56b2b2eef0a59ef1c
John Solly <9572232+jsolly@users.noreply.github.com>
2024-10-01 12:55:20 -0400

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercelServerless from "@astrojs/vercel/serverless";
+import vercelStatic from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://hackmycareer.lol",
 	integrations: [tailwind()],
-	output: "server",
-	adapter: vercelServerless(),
+	output: "static",
+	adapter: vercelStatic(),
 });
