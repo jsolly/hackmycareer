c4bf42239b3293961a533faf66c2d26fedcc1b6d
John Solly <9572232+jsolly@users.noreply.github.com>
2024-11-30 20:26:52 -0500

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercelStatic from "@astrojs/vercel/static";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://hackmycareer.lol",
-	integrations: [tailwind()],
+	integrations: [tailwind(), sitemap()],
 	output: "static",
 	adapter: vercelStatic(),
 });
